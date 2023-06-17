8456143196f8ceebf85d31f096826c3fed30a7f5
saba satseradze <saba.satseradze0@gmail.com>
2023-06-16 17:50:56 -0700

    init Todo-list-app

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './style.css';
+import { NewTodoForm } from './newTodoForm';
+import { TodoList } from './TodoList';
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false }
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className='header'>Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    </>
+  )
+}
